46e337e009c91e7f88312da9a2e053ffc2da5d22
Nico Thiebes <nico.thiebes@gmail.com>
2014-05-30 21:16:52 +0200

    new zombie attributes

--- js/src/player.js
+++ js/src/player.js
@@ -16,13 +16,13 @@ var playerAttr = {
 
 	zombie: {
 		type: 'melee',
-		health: 100,
+		health: 75,
 		healthGeneration: 5,
 		damage: 10,
 		spriteOffset: 604,
 	    spriteHeight: 46,
 	    hitbox: [61, 27],
-	    walkAnimationSpeed: 5.5,
-	    fightAnimationSpeed: 6.5
+	    walkAnimationSpeed: 4.5,
+	    fightAnimationSpeed: 5.5
 	}
 };
